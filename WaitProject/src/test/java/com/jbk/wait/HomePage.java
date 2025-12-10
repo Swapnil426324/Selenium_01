44dfad255e14085ec3d6e0927073e046d0c9f367
Swapnil Alaskar <alaskarswapnil1998@gmail.com>
2025-12-10 17:17:54 +0530

    second change in homepage

---
+++ WaitProject/src/test/java/com/jbk/wait/HomePage.java
@@ -0,0 +1,16 @@
+package com.jbk.wait;
+
+import org.testng.annotations.Test;
+
+public class HomePage {
+
+	@Test
+	public void test01() {
+		System.out.println("this is test01");
+	}
+	
+	@Test
+	public void test02() {
+		System.out.println("this is test02");
+	}
+}
